5f6b9aafec4a39f32ad86c62c1a51fa3057879e5
ClementBarreda <133324408+ClementBarreda@users.noreply.github.com>
2023-06-12 11:47:12 +0200

    ajout de plus de commentaires + traduction

--- script.sh
+++ script.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
-# Informations de connexion à la base de données MySQL
+# MySQL database connection information
 host=lhcp3349.webapps.net
 user=iv5g2mc0_admin
 password=Motdepasse31!
 port=3306
 database=iv5g2mc0_sae21
 
-# Callback exécutée lors de la connexion au broker MQTT
+# Function executed when connecting to the MQTT broker
 on_connect() {
-    echo "Connexion au broker MQTT établie avec le code de résultat : $1"
-    # S'abonner aux topics lors de la connexion
+    echo "Connection to the MQTT broker established with result code: $1"
+    # Subscribing to topics upon connection
     mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t "Student/by-room/E001/data" &
     mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t "Student/by-room/E105/data" &
     mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t "Student/by-room/B202/data" &
     mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t "Student/by-room/B203/data" &
 }
 
-# Callback exécutée lors de la réception d'un message MQTT
+# Function executed upon receipt of an MQTT message
 on_message() {
-    echo "Message reçu sur le topic $1:"
+    echo "Message received on topic $1:"
     payload="$2"
 
-    # Extraction des champs requis
+    # Extraction of required fields
     temperature=$(echo "$payload" | jq -r '.[0].temperature')
     room=$(echo "$payload" | jq -r '.[1].room')
     building=$(echo "$payload" | jq -r '.[1].Building')
     devEUI=$(echo "$payload" | jq -r '.[1].devEUI')
 
-    # Récupération de l'heure et la date actuelles
+    # Getting current time and date
     horaire=$(date +"%H:%M:%S")
     date=$(date +"%d/%m/%Y")
 
-    # Insertion des données dans la base de données
+    # Inserting the data into the database
     query="INSERT INTO MESURE (date, horaire, valeur, capteur) VALUES ('$date', '$horaire', '$temperature', '$devEUI')"
     mysql -h "$host" -u "$user" -p"$password" -P "$port" -D "$database" -e "$query"
 
 }
 
-# Connexion au broker MQTT et configuration des callbacks
+# "Connecting to the MQTT broker and setting up callbacks based on the topic + creating the "topic" and "message" variables using the "awk" command"
 mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t "#" -v | while read -r line; do
     topic=$(echo "$line" | awk '{print $1}')
     message=$(echo "$line" | awk '{print $2}')
